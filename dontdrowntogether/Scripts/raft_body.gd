816228454e2fdd8c726e99fc7e9201ec83e5fbe8
Viktor-Domenstam <56826979+Viktor-Domenstam@users.noreply.github.com>
2024-10-25 21:30:31 +0200

    Added initial script for tiles and raft
    push

---
+++ dontdrowntogether/Scripts/raft_body.gd
@@ -0,0 +1,19 @@
+extends Node
+
+var row = 3
+var column = 3
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+func _create_grid() -> void:
+	#for r in row:
+		#for c in column:
+			#Instantiate tiles in a 3x3 grid.
+	pass
