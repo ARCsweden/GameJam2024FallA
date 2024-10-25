34e95ff1b90a3e99bb0beaa6e75f88fac10ae3d7
Seasnakez <adde456789@gmail.com>
2024-10-25 23:29:31 +0200

    Added _create_grid()

--- dontdrowntogether/Scripts/raft_body.gd
+++ dontdrowntogether/Scripts/raft_body.gd
@@ -1,11 +1,15 @@
 extends Node
 
-var row = 3
-var column = 3
+# Preloads scene for faster loading
+var RaftTileScene := load("res://Scenes/RaftTile.tscn")
+
+var row: int = 3
+var column: int = 3
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	_create_grid()
+	#pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,7 +17,17 @@ func _process(delta: float) -> void:
 	pass
 	
 func _create_grid() -> void:
-	#for r in row:
-		#for c in column:
-			#Instantiate tiles in a 3x3 grid.
+	#Instantiate tiles in a 3x3 grid.
+	var instance
+	for r in row:
+		print(r)
+		for c in column:
+			# Creates an instance of RaftTileScene
+			instance = RaftTileScene.instantiate() 
+			# Adds the instance to the scene tree
+			add_child(instance)
+			# Moves the instance. +50 is to move the image coordinate to the top left corner
+			# Then each instance is moved to the top left corner of the collision shape, to center it
+			instance.position = Vector2(r*100+50, c*100+50) - ($RaftCollisionShape.shape.size / 2)
+			
 	pass
