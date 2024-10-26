5495a0d7c18dcc8b5652364ba911c8f00c10a6d9
Per Lenander <per.lenander@addiva.se>
2024-10-26 15:40:12 +0200

    Made it possible to pick up grunkor, with different values. Number is currently kept in hud

--- dontdrowntogether/Scenes/grunka.gd
+++ dontdrowntogether/Scenes/grunka.gd
@@ -1,6 +1,8 @@
 extends RigidBody2D
 class_name Grunka
 
+var value: int = 1
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	SignalBus.hooked.connect(_on_hooked)
@@ -8,4 +10,5 @@ func _ready() -> void:
 
 func _on_hooked(body) -> void:
 	if body == self:
+		SignalBus.pickup_grunka.emit(value)
 		queue_free()
--- dontdrowntogether/Scripts/game.gd
+++ dontdrowntogether/Scripts/game.gd
@@ -21,17 +21,21 @@ func _ready():
 	if connected.size() == 0:
 		create_player(0)
 
+	# Delay first grunka spawn by a few seconds
 	await get_tree().create_timer(3.0).timeout
-	print("test")
 	grunk_timer.connect("timeout", _on_grunk_spawn)
 	grunk_timer.start(randf_range(1.0, 3.0))
 
 func _on_grunk_spawn() -> void:
 	var grunka: Grunka = GrunkaScene.instantiate()
+	var value = randi_range(1, 5)
+	grunka.value = value
 	grunka.position = Vector2(randf_range(100, 1900), -100)
 	grunka.angular_velocity = randf_range(-1.0, 1.0)
 	grunka.linear_velocity = Vector2(randf_range(-50.0, 50.0), randf_range(100.0, 300.0))
 	add_child(grunka)
+	# Randomize timer
+	grunk_timer.start(randf_range(1.0, 3.0))
 
 func create_player(i: int) -> void:
 	var player = PlayerScene.instantiate()
