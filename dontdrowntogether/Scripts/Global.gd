6f945d745bce0578b6acc02b3402bf8d28219022
Viktor-Domenstam <56826979+Viktor-Domenstam@users.noreply.github.com>
2024-10-26 17:50:00 +0200

    Added repairable tiles that you stand on
    Added repairable tiles that you stand on
    Added a scrap amount required to repair a tile

--- dontdrowntogether/Scripts/Global.gd
+++ dontdrowntogether/Scripts/Global.gd
@@ -6,3 +6,4 @@ var rng = RandomNumberGenerator.new()
 
 # Player
 var scrapAmount : int = 0
+var repair_cost : int = 10
--- dontdrowntogether/Scripts/player.gd
+++ dontdrowntogether/Scripts/player.gd
@@ -14,6 +14,9 @@ var paddle
 var hook_btn
 var controller_ready := false
 
+var last_repairable_tile
+var can_repair = false
+
 var cur_dir: Vector2 = Vector2.LEFT
 
 func _physics_process(_delta: float) -> void:
@@ -46,15 +49,22 @@ func _process(_delta) -> void:
 	if controller_ready == true:
 		if Input.is_action_just_pressed("hook" + str(controller_id)):
 			hook.activate_hook(cur_dir)
+		if Input.is_action_just_pressed("DebugRepair"): #TODO Change to an actual button for a controller
+			if(can_repair):
+				last_repairable_tile.call_repair()
 
 func repair_raft_tile() -> void:
 	pass
 
 func _on_damage_tile_entered(_area):
-	$PlayerBoundUi/Label.text = "PRESS [BUTTON] TO REPAIR"
-	$PlayerBoundUi/Label.visible = true
+	if(Global.scrapAmount >= Global.repair_cost):
+		$PlayerBoundUi/Label.text = "PRESS [BUTTON] TO REPAIR"
+		$PlayerBoundUi/Label.visible = true
+		last_repairable_tile = _area
+		can_repair = true
 	pass
 
 func _on_repair_check_area_area_exited(area: Area2D) -> void:
 	$PlayerBoundUi/Label.visible = false
+	can_repair = false
 	pass
