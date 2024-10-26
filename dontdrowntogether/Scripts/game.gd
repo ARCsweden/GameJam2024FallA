9a8bd1b02aa832e1887b588374ff7807996dbabb
Erik <31732187+eln17011@users.noreply.github.com>
2024-10-26 10:54:02 +0200

    Controller_id Logic
    Lade till logik för olika kontrollids

--- dontdrowntogether/Scripts/game.gd
+++ dontdrowntogether/Scripts/game.gd
@@ -8,10 +8,33 @@ func _ready():
 	var player1 = PlayerScene.instantiate()
 	var player2 = PlayerScene.instantiate()
 
+	#TODO: Spawn in raft
+	#TODO: Get spawnpoints for player 1 and player 2 from raft
+	
 	# Set positions or other properties if necessary
 	player1.position = Vector2(100, 100)  # Adjust as needed
 	player2.position = Vector2(300, 100)  # Adjust as needed
-
-	# Add instances to the scene tree
+	
+	
 	add_child(player1)
 	add_child(player2)
+# Controller ID for this player
+
+
+func get_unique_controller_id() -> int:
+	var connected = Input.get_connected_joypads()
+	# Check if there are any connected controllers
+	if connected.size() >= 2:
+		# Assign the first controller ID to Player 1 and second to Player 2
+		if is_instance_valid(self) and is_in_group("Player1"):
+			return connected[0]
+		else:
+			return connected[1]
+	else:
+		print("Not enough controllers connected!")
+		return -1
+		
+func _process(delta):
+	pass
+	# Example movement input (adjust based on your input mappings)
+	# Add instances to the scene tree
