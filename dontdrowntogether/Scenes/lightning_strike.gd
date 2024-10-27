5f9585a737d12a7e7a3c3e918827390ec84b4a4a
Per Lenander <per.lenander@addiva.se>
2024-10-27 13:43:58 +0100

    Lightning strike

---
+++ dontdrowntogether/Scenes/lightning_strike.gd
@@ -0,0 +1,18 @@
+extends DirectionalLight2D
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var timer: Timer = $Timer
+
+const timeout_min : float = 10.0
+const timeout_max : float = 30.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	timer.timeout.connect(_on_timeout)
+	timer.start(randf_range(timeout_min, timeout_max))
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _on_timeout() -> void:
+	animation_player.play("strike")
+	timer.start(randf_range(timeout_min, timeout_max))
